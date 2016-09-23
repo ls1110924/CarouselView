85b4b8b4fecf287c07649cdff750c2c4750db61a
A Shuai <ls1110924@163.com>
2016-09-23 12:25:23 +0800

    debug click listener on carouselview

--- app/src/main/java/com/yunxian/carousel/app/TestCarouselActivity.java
+++ app/src/main/java/com/yunxian/carousel/app/TestCarouselActivity.java
@@ -1,11 +1,17 @@
 package com.yunxian.carousel.app;
 
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 
 import com.yunxian.carousel.BaseCarouselAdapter;
 import com.yunxian.carousel.CarouselView;
 import com.yunxian.carousel.app.adapter.TestCustomAdapter;
+import com.yunxian.carousel.app.adapter.TestJustEnoughAdapter;
+import com.yunxian.carousel.app.adapter.TestNotEnoughAdapter;
+import com.yunxian.carousel.app.adapter.TestOneLineAdapter;
+import com.yunxian.carousel.app.adapter.TestTwoLineEnoughAdapter;
+import com.yunxian.carousel.app.adapter.TestTwoLineNotEnoughAdapter;
 import com.yunxian.carousel.app.utils.BaseFragmentActivity;
 
 import java.util.ArrayList;
@@ -19,6 +25,8 @@ import java.util.Locale;
  */
 public class TestCarouselActivity extends BaseFragmentActivity {
 
+    private static final String TAG = TestCarouselActivity.class.getSimpleName();
+
     private CarouselView mCarouselViewOne;
     private CarouselView mCarouselViewTwo;
     private CarouselView mCarouselViewThree;
@@ -29,6 +37,8 @@ public class TestCarouselActivity extends BaseFragmentActivity {
     private int startIndex;
     private final List<String> mCustomData = new ArrayList<>();
     private BaseCarouselAdapter mCustomAdapter;
+
+    private final CommonCallbackListener mCommonListener = new CommonCallbackListener();
 
     @Override
     protected void onSetContentView() {
@@ -49,19 +59,20 @@ public class TestCarouselActivity extends BaseFragmentActivity {
 
     @Override
     protected void onBindContent() {
-//        mCarouselViewOne.setAdapter(new TestNotEnoughAdapter(this));
-//        mCarouselViewTwo.setAdapter(new TestJustEnoughAdapter(this));
-//        mCarouselViewThree.setAdapter(new TestOneLineAdapter(this));
-//        mCarouselViewFour.setAdapter(new TestTwoLineEnoughAdapter(this));
-//        mCarouselViewFive.setAdapter(new TestTwoLineNotEnoughAdapter(this));
+        mCarouselViewOne.setAdapter(new TestNotEnoughAdapter(this));
+        mCarouselViewTwo.setAdapter(new TestJustEnoughAdapter(this));
+        mCarouselViewThree.setAdapter(new TestOneLineAdapter(this));
+        mCarouselViewFour.setAdapter(new TestTwoLineEnoughAdapter(this));
+        mCarouselViewFive.setAdapter(new TestTwoLineNotEnoughAdapter(this));
 
         startIndex = 0;
-        for (int i = startIndex; i < startIndex + 2; i++) {
+        for (int i = startIndex; i < startIndex + 3; i++) {
             mCustomData.add(String.format(Locale.US, "Test--->%1$d", i));
         }
         startIndex += 2;
         mCustomAdapter = new TestCustomAdapter(this, mCustomData);
         mCarouselViewSix.setAdapter(mCustomAdapter);
+        mCarouselViewSix.setOnItemClickListener(mCommonListener);
     }
 
     @Override
@@ -92,5 +103,18 @@ public class TestCarouselActivity extends BaseFragmentActivity {
         return true;
     }
 
+    private class CommonCallbackListener implements CarouselView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(CarouselView parent, View view, int position, long id) {
+            Log.d(TAG, "Click--->" + position);
+        }
+
+        @Override
+        public boolean onItemLongClick(CarouselView parent, View view, int position, long id) {
+            Log.d(TAG, "LongClick--->" + position);
+            return true;
+        }
+    }
 
 }
